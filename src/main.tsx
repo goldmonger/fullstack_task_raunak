2e8a3e182b97ee7e7826e45ea73d2ff5a8a19034
goldmonger <raunakhebballi@gmail.com>
2025-05-09 15:55:41 +0530

    build: added react, eslint, tailwind

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { Container, createRoot } from "react-dom/client";
+import React from "react";
+// import './index.css'
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root") as Container).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import type { UserConfig } from "vite";
+
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+}) satisfies UserConfig;

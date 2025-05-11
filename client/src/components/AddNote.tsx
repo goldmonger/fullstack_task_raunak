25d6d5425364d19f506977f176c3e03bd421d1f3
goldmonger <raunakhebballi@gmail.com>
2025-05-11 20:23:25 +0530

    fix: responsive form, layout

--- client/src/components/AddNote.tsx
+++ client/src/components/AddNote.tsx
@@ -7,21 +7,21 @@ interface IAddNoteProps {
 const AddNote = ({ submit }: IAddNoteProps) => {
   return (
     <div className="pt-2 pb-2 3xl:pb-5">
-      <div className=" flex gap-5 w-full">
+      <div className="flex flex-col items-end md:items-center md:flex-row gap-3 md:gap-5 w-full">
         <input
           type="text"
           placeholder="New Note..."
           id="form-input"
-          className="w-3/4 border border-[#D3D4D9] rounded-xl shadow 3xl:text-[30px] px-4 py-2 placeholder:font-normal placeholder:text-2xl"
+          className="w-full md:w-3/4 border border-[#D3D4D9] rounded-xl shadow text-[17px] text-[20px] 3xl:text-[30px] px-4 pt-2 pb-1 md:pt-2.5 md:pb-1.5 3xl:pt-1.5 3xl:pb-1 placeholder:font-normal placeholder:text-2xl"
         />
         <button
-          className="border lg:text-[24px] 3xl:text-[28px] rounded-xl w-1/4 flex items-center justify-center gap-3 bg-[#92400E] text-white"
+          className="border w-1/2 md:w-1/4 py-1 md:py-2 px-4 text-[20px] 3xl:text-[28px] rounded-xl flex items-center justify-center gap-3 bg-[#92400E] text-white"
           onClick={submit}
         >
-          <div className="rounded-full w-7 h-7 bg-white flex items-center justify-center">
+          <div className="rounded-full w-4 h-4 3xl:w-7 3xl:h-7 bg-white flex items-center justify-center">
             <div className="relative w-4 h-0.5">
-              <div className="w-4 h-0.5 bg-[#92400E] absolute top-0 left-0"></div>
-              <div className="w-4 h-0.5 bg-[#92400E] absolute top-0 left-0 rotate-90"></div>
+              <div className="w-2 h-0.5 3xl:w-4 3xl:h-0.5 bg-[#92400E] absolute left-1 3xl:top-0 3xl:left-0"></div>
+              <div className="w-2 h-0.5 3xl:w-4 3xl:h-0.5 bg-[#92400E] absolute left-1 3xl:top-0 3xl:left-0 rotate-90"></div>
             </div>
           </div>
           Add
--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -7,14 +7,14 @@ interface ILayoutProps {
 const Layout = ({ children }: ILayoutProps) => {
   return (
     <div className="flex items-center justify-center h-screen w-full">
-      <div className="border border-[#D3D4D9] font-inter font-semibold rounded-lg shadow 3xl:px-6">
+      <div className="max-w-[900px] mx-3 border border-[#D3D4D9] font-inter font-semibold rounded-lg shadow px-6">
         {/* context for connect status */}
         {/* <p className="text-emerald-500 text-xl">{connectStatus}</p> */}
-        <div className="pt-5 pb-1 rounded-t-lg flex items-center">
+        <div className="pt-5 pb-1 rounded-t-lg flex items-center gap-1 md:gap-2 relative -left-2">
           <div className="h-16 w-16 rounded-lg relative">
-            <img src="/logo.png" className="absolute -top-0.5 -left-2.5" />
+            <img src="/logo.png" className="" />
           </div>
-          <div className="lg:text-[32px] 3xl:text-[48px]">Note App</div>
+          <div className="text-[24px] md:text-[32px] 3xl:text-[48px]">Note App</div>
         </div>
         {children}
       </div>

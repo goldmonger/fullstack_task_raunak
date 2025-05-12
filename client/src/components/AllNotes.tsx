91e74cd349b4b8365206e9485ecc7be74fd0f9b2
goldmonger <raunakhebballi@gmail.com>
2025-05-12 20:31:26 +0530

    fix: scrollbar style

--- client/src/components/AllNotes.tsx
+++ client/src/components/AllNotes.tsx
@@ -1,17 +1,26 @@
 import React, { useContext } from "react";
 import NotesContext from "../contexts/NotesContext";
 
-
 const AllNotes = () => {
   // write hook to get all notes here
   const allNotes = useContext(NotesContext)["tasks"];
 
   return (
-    <div className="3xl:text-[30px] lg:text-[22px] 3xl:pt-3">
-      <h1 className="font-bold border-b border-[#C5CAD3]">Notes</h1>
-      <div className="font-normal max-h-[24vh] overflow-y-scroll">
+    <div className="3xl:text-[30px] lg:text-[22px] 3xl:pt-3 pb-5">
+      <h1 className="font-bold py-1">Notes</h1>
+      <div className="font-normal max-h-[24vh] flex flex-col no-scrollbar">
         {allNotes && allNotes.length > 0 ? (
           allNotes.map((el, idx) => {
+            if (idx === 0) {
+              return (
+                <div
+                  key={idx}
+                  className="border-b border-t border-[#C5CAD3] py-3"
+                >
+                  {el}
+                </div>
+              );
+            }
             return (
               <div key={idx} className="border-b border-[#C5CAD3] py-3">
                 {el}
@@ -19,7 +28,7 @@ const AllNotes = () => {
             );
           })
         ) : (
-          <p>No Notes Yet..</p>
+          <p className="py-2">Add a new note.</p>
         )}
       </div>
     </div>
